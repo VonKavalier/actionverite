b76bc1d8e7c4a786b93f2eb8532bc6e077cf076a
Tom <von@disroot.org>
2019-12-26 12:39:00 +0100

    Ajout de jquery et d'une progressbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,29 @@
 function get_gage(list){
-    var gage = list.random();
-    document.getElementById('gage').innerHTML = gage;
+    var timerId, percent;
+
+    // reset progress bar
+    percent = 0;
+    $('#btn').attr('disabled', true);
+    $('.progress-bar').css('width', '0px').addClass('progress-bar-animated active');
+
+    timerId = setInterval(function() {
+
+        // increment progress bar
+        percent += 1;
+        $('.progress-bar').css('width', percent + '%');
+        $('.progress-bar').html("Calcul de l'humliation en cours... " + percent + '%');
+
+        if (percent >= 100) {
+            clearInterval(timerId);
+            $('#btn').attr('disabled', false);
+            $('.progress-bar').removeClass('progress-bar-animated progress-bar-striped active').html();
+            var gage = list.random();
+            document.getElementById('gage').innerHTML = gage;
+        }
+    }, 100);
 }
 
 Array.prototype.random = function () {
-       return this[Math.floor((Math.random()*this.length))];
+    return this[Math.floor((Math.random()*this.length))];
 }
+
