15da6b8c4dbfd3e3b88ec088b40eae325df39d73
Tom <von@disroot.org>
2019-12-26 14:42:34 +0100

    Gestion du reset et meilleur affichage des cards

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 function get_gage(list){
+    $('.progress').show();
+    $('#gage').hide();
+
     var timerId, percent;
 
     // reset progress bar
     percent = 0;
-    $('#btn').attr('disabled', true);
-    $('.progress-bar').css('width', '0px').addClass('progress-bar-animated active');
+    $('.btn').attr('disabled', true);
 
     timerId = setInterval(function() {
 
@@ -16,11 +18,15 @@ function get_gage(list){
         if (percent >= 100) {
             clearInterval(timerId);
             $('#btn').attr('disabled', false);
-            $('.progress-bar').removeClass('progress-bar-animated progress-bar-striped active').html();
+            $('.progress-bar').removeClass('progress-bar-animated progress-bar-striped active');
+            $('.progress').hide();
             var gage = list.random();
+            $('#gage').show();
             document.getElementById('gage').innerHTML = gage;
+            $('.btn').attr('disabled', false);
+            $('.progress-bar').css('width', '0px').addClass('progress-bar-animated active');
         }
-    }, 100);
+    }, 80);
 }
 
 Array.prototype.random = function () {
